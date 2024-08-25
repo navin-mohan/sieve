a46aaf2a0e8316efe1c73df9e1922f79fc67e338
Navin Mohan <navinmohan81@gmail.com>
2024-08-24 23:52:20 -0400

    Use StreamWordCounter in SerialWordFrequencyCalculator

--- app/services/utils/SerialWordFrequencyCalculator.java
+++ app/services/utils/SerialWordFrequencyCalculator.java
@@ -1,18 +1,19 @@
 package services.utils;
 
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 public class SerialWordFrequencyCalculator implements WordFrequencyCalculator {
 
     protected HashMap<String, Integer> wordFrequency = new HashMap<>();
     @Override
     public void addString(String str) {
-        final StringTokenizer tokenizer = new StringTokenizer(str, " ,.:;\n\t");
-        while (tokenizer.hasMoreTokens()) {
-            final String word = tokenizer.nextToken();
-            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
-        }
+
+        Stream<Character> stream = (str + " ").chars().mapToObj(c -> (char) c);
+        StreamWordCounter streamWordCounter = stream.collect(() -> new StreamWordCounter(true, "", new HashMap<>()),
+                StreamWordCounter::accumulate,
+                StreamWordCounter::combine);
+        wordFrequency = streamWordCounter.getWordFrequency();
     }
 
     @Override
