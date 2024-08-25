d2b84d08ece0112806577c6a4fb3a761e2fdcb37
Navin Mohan <navinmohan81@gmail.com>
2024-08-25 03:48:40 -0400

    Wire up the actors and update routes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,12 +1,27 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.pekko.actor.ActorRef;
+import org.apache.pekko.actor.ActorSystem;
+import org.apache.pekko.util.FutureConverters;
+import play.libs.Json;
 import play.mvc.*;
+import static org.apache.pekko.pattern.Patterns.ask;
+import javax.inject.Inject;
+import java.util.concurrent.CompletionStage;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    final ActorRef textProcessingActor;
+
+    @Inject
+    public HomeController(ActorSystem system) {
+        this.textProcessingActor = system.actorOf(services.actors.TextProcessingActor.getProps(), "textProcessingActor");
+    }
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -18,12 +33,36 @@ public class HomeController extends Controller {
         return ok(views.html.index.render());
     }
 
-    public Result about() {
-        return ok("About Us");
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result ingest(Http.Request request) {
+        Http.RequestBody body = request.body();
+        final String text = body.asJson().get("text").asText();
+        final boolean isParallel = body.asJson().get("parallel").asBoolean();
+
+        textProcessingActor.tell(new services.actors.TextProcessingActorProtocol.GetWordFrequency(text, isParallel), ActorRef.noSender());
+
+        return ok();
     }
 
-    public Result ingest() {
-        return ok();
+    public CompletionStage<Result> get10MostFrequentWords() {
+        return FutureConverters.asJava(
+                ask(textProcessingActor, new services.actors.Top10MostFrequentWordsActorProtocol.GetWords(), 1000))
+                .thenApply(response -> {
+                    ObjectNode json = Json.newObject();
+                    json.put("words", Json.toJson(response));
+                    return ok(json);
+                });
+    }
+
+    public CompletionStage<Result> get10LeastFrequentWords() {
+        return FutureConverters.asJava(
+                ask(textProcessingActor, new services.actors.Top10LeastFrequentWordsActorProtocol.GetWords(), 1000))
+                .thenApply(response -> {
+                            ObjectNode json = Json.newObject();
+                            json.put("words", Json.toJson(response));
+                            return ok(json);
+                        });
     }
 
 }
